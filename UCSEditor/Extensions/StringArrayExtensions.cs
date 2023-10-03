36b8612fecc65b40d504269816e7ea8e910b83e4
Matheus Vilano <62972455+matheusvilano@users.noreply.github.com>
2023-10-03 03:13:37 -0700

    Updated documentation for StringArrayExtensions.cs

--- UCSEditor/Extensions/StringArrayExtensions.cs
+++ UCSEditor/Extensions/StringArrayExtensions.cs
@@ -1,12 +1,25 @@
 ﻿namespace UCSEditor.Extensions
 {
+    /// <summary>
+    /// Extensions for the string[] type.
+    /// </summary>
     internal static class StringArrayExtensions
     {
+        /// <summary>
+        /// Gets the last valid index in the string array.
+        /// </summary>
+        /// <param name="str">The caller object, which is an instance of the `string` class.</param>
+        /// <returns>The last valid index.</returns>
         public static int GetLastIndex(this string[] str)
         {
             return str.Length - 1;
         }
 
+        /// <summary>
+        /// Gets the last valid object in the string array.
+        /// </summary>
+        /// <param name="str">The caller object, which is an instance of the `string` class.</param>
+        /// <returns>The last valid object.</returns>
         public static string GetLastObject(this string[] str)
         {
             return str[str.Length - 1];
