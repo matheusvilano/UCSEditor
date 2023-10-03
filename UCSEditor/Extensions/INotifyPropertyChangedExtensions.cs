568819565e55c034684416953dfa9284847e972b
Matheus Vilano <62972455+matheusvilano@users.noreply.github.com>
2023-10-03 03:11:27 -0700

    Updated documentation for INotifyPropertyChangedExtensions.cs

--- UCSEditor/Extensions/INotifyPropertyChangedExtensions.cs
+++ UCSEditor/Extensions/INotifyPropertyChangedExtensions.cs
@@ -3,8 +3,17 @@ using System.Runtime.CompilerServices;
 
 namespace UCSEditor.Extensions
 {
+    /// <summary>
+    /// Extensions for the INotifyPropertyChanged interface.
+    /// </summary>
     internal static class INotifyPropertyChangedExtensions
     {
+        /// <summary>
+        /// Invokes a PropertyChanged event via the specified handler.
+        /// </summary>
+        /// <param name="obj">The caller object, which is an instance of a class that implements `INotifyPropertyChanged`.</param>
+        /// <param name="evt">The PropertyChanged event handler.</param>
+        /// <param name="propertyName">The name of the property got assigned a new value. It is recommended to use the default value here, which is the caller member name (automatically fetched via the `CallerMemberName` attribute.</param>
         public static void OnPropertyChanged(this INotifyPropertyChanged obj, in PropertyChangedEventHandler evt, [CallerMemberName] in string propertyName = "")
         {
             evt?.Invoke(obj, new PropertyChangedEventArgs(propertyName));
