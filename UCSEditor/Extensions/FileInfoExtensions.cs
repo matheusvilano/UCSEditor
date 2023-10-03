49fcd66ad7cc34c84c7786d3756195f09867eb9e
Matheus Vilano <62972455+matheusvilano@users.noreply.github.com>
2023-10-03 03:06:30 -0700

    Updated documentation for FileInfoExtensions.cs

--- UCSEditor/Extensions/FileInfoExtensions.cs
+++ UCSEditor/Extensions/FileInfoExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace UCSEditor.Extensions
 {
+    /// <summary>
+    /// Entensions for the FileInfo class.
+    /// </summary>
     public static class FileInfoExtensions
     {
+        /// <summary>
+        /// Renames the file represented by the FileInfo object.
+        /// </summary>
+        /// <param name="fileInfo">The caller object, which is an instance of the FileInfo class.</param>
+        /// <param name="newName">The new name.</param>
         public static void Rename(this FileInfo fileInfo, in string newName)
         {
             fileInfo.MoveTo(Path.Combine(fileInfo.Directory.FullName, newName));
