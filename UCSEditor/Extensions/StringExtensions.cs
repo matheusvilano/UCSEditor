a6e220732831b3dd21cb2791cd9050667bb02498
Matheus Vilano <62972455+matheusvilano@users.noreply.github.com>
2023-10-03 03:18:05 -0700

    Updated documentation for StringExtensions.cs

--- UCSEditor/Extensions/StringExtensions.cs
+++ UCSEditor/Extensions/StringExtensions.cs
@@ -1,7 +1,15 @@
 ﻿namespace UCSEditor.Extensions
 {
+    /// <summary>
+    /// Extensions for the `String` class.
+    /// </summary>
     internal static class StringExtensions
     {
+        /// <summary>
+        /// Returns a version of the string that contains only letters.
+        /// </summary>
+        /// <param name="str">The caller object, which is an instance of the `string` class.</param>
+        /// <returns>The string, but containing letters only.</returns>
         public static string KeepOnlyLetters(this string chars)
         {
             foreach (char character in chars)
@@ -14,6 +22,11 @@
             return chars;
         }
 
+        /// <summary>
+        /// Returns a version of the string that contains only letters and digits.
+        /// </summary>
+        /// <param name="str">The caller object, which is an instance of the `string` class.</param>
+        /// <returns>The string, but containing letters and digits only.</returns>
         public static string KeepOnlyLettersAndDigits(this string chars)
         {
             foreach (char character in chars)
@@ -26,6 +39,12 @@
             return chars;
         }
 
+        /// <summary>
+        /// Returns a version of the string that does not contain any whitespace. A replacement for whitespace can be specified.
+        /// </summary>
+        /// <param name="str">The caller object, which is an instance of the `string` class.</param>
+        /// <param name="replacement">The character to replace any whitespace. By default, whitespace characters are replaced by underscores.</param>
+        /// <returns>The string, but with any whitespaced replaced by an underscore (or the specified replacement).</returns>
         public static string ReplaceWhitespace(this string chars, in char replacement = '_')
         {
             foreach (char character in chars)
